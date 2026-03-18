0cfc39de00c924a494d7a4381c34cb89be6bc14a
Gabriel Wak <gabrieltrabalho199@gmail.com>
2026-03-18 15:42:51 -0300

    fix supabase env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "SUA_URL_AQUI";
+const supabaseAnonKey = "SUA_KEY_AQUI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
